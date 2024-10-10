10d9daa02005ddec799129dbeb7a5786dd60a22e
Dimitry <dimitrymmarinho@gmail.com>
2024-10-10 12:59:24 -0300

    feat: :art: Criação de script para manipulação da pagina

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,32 @@
-function toggleMenu() {
-  const menu = document.getElementById("menu");
-  menu.classList.toggle("active");
-}
+window.addEventListener("scroll", () => {
+  const elements = document.querySelectorAll(".about-text, .motivation-text");
+
+  elements.forEach((element) => {
+    const position = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (position < windowHeight && position > 0) {
+      element.classList.add("animate");
+    } else {
+      element.classList.remove("animate");
+    }
+  });
+});
+document.querySelectorAll(".link-menu").forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    const targetId = this.getAttribute("href").substring(1);
+    document
+      .getElementById(targetId)
+      .scrollIntoView({ behavior: "smooth", block: "start" });
+  });
+  document.removeEventListener();
+});
+// document
+//   .getElementById("link-menu")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+//     document
+//       .getElementById("about-section")
+//       .scrollIntoView({ behavior: "smooth" });
+//   });
